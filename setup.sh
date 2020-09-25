431e96c397dbeb216c1ea72ca90fe4f7923e1ac6
Ben Mitchell <bjosephmitchell@gmail.com>
2020-09-25 14:48:44 +0100

    Added shell script for gitconfig

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # File symlinks
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 
 # Dir symlinks
 ln -s $HOME/dotfiles/bin $HOME/bin
