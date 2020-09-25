c8e4d82a91229f3b61ba8b9b0f2e714574e9e3b1
Ben Mitchell <bjosephmitchell@gmail.com>
2020-09-25 14:39:56 +0100

    Added a setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# File symlinks
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
+
+# Dir symlinks
+ln -s $HOME/dotfiles/bin $HOME/bin
+ln -s $HOME/dotfiles/.zsh-plugins $HOME/.zsh-plugins
