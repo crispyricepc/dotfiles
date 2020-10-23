d26b239330860c6771c5657436f911b96c2f6c32
Ben Mitchell <bjosephmitchell@gmail.com>
2020-10-23 16:11:48 +0100

    I wasn't aware of all the redundant semicolons but ok

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ! command -v zsh > /dev/null || ! command -v bat > /dev/null;
+if ! command -v zsh > /dev/null || ! command -v bat > /dev/null
 then
     echo -e "The programs 'bat' and 'zsh' are required for these dotfiles to work properly.
 Everything will install correctly without the program(s) installed, but you won't be able to use some of the features until they are.\n"
@@ -13,7 +13,7 @@ Everything will install correctly without the program(s) installed, but you won'
 fi
 
 echo "Fetching and rebasing"
-if ! (git fetch && git rebase origin master);
+if ! (git fetch && git rebase origin master)
 then
     echo "Rebase failed, is your local repository clean?"
     exit 1
@@ -21,7 +21,7 @@ fi
 echo "Rebase succeeded"
 
 echo "Updating submodules..."
-if ! git submodule update --init --recursive;
+if ! git submodule update --init --recursive
 then
     echo "Failed to update submodules! Try doing a fresh clone of this repository. If it still doesn't work, submit an issue"
     exit 1
