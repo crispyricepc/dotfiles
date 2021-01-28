aee19261e2d6d33af08fe5dc12c1e445eb533fd6
Ben Mitchell <bjosephmitchell@gmail.com>
2021-01-28 12:52:13 +0000

    Added ruby gems to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,7 @@ function __fish_default_command_not_found_handler
 end
 
 # Set path bins for cargo and poetry
-set -x PATH $HOME/.cargo/bin $HOME/.poetry/bin $HOME/.local/bin $HOME/bin $PATH
+set -x PATH $HOME/.cargo/bin $HOME/.poetry/bin $HOME/.gem/ruby/2.7.0/bin $HOME/.local/bin $HOME/bin $PATH
 
 # Disable fish greeting
 set fish_greeting
