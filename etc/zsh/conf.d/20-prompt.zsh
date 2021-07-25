34c835739c7879974f44bd36252d4b3e1dc30274
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-25 10:29:18 +0000

    zsh global configs

---
+++ etc/zsh/conf.d/20-prompt.zsh
@@ -0,0 +1,7 @@
+setopt PROMPT_SUBST
+
+prompt() {
+     PROMPT="[%F{magenta}%n%f@%F{yellow}%m%f:%F{blue}%~%f] $(__git_ps1 "(%s) ")%(?.%F{green}%(!.#.$)%f.%F{red}%? %(!.#.$)%f) "
+}
+
+precmd_functions+=(prompt)
