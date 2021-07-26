692a24adb882a6a1a58127e821da6703f1007c0a
Ben Mitchell <bjosephmitchell@gmail.com>
2021-07-26 12:00:06 +0000

    New polybar config

---
+++ config/polybar/launch_polybar.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env sh
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
