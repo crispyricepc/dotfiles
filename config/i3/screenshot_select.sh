4618d31a820d6c3af9c3c72e855d3f52c8950747
Ben <bjosephmitchell@gmail.com>
2022-01-20 16:21:41 +0000

    Remove i3 config

--- config/i3/screenshot_select.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p $(xdg-user-dir PICTURES)/screenshots
-IMG=$(xdg-user-dir PICTURES)/screenshots/$(date "+%s").png
-
-maim --select $IMG &&\
-	xclip -selection clipboard -t image/png -i $IMG
-
-notify-send "Screenshot copied to clipboard" "You can find the screenshot at $IMG" -t 3000
