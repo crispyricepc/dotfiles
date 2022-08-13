ac100c939f04bc1901beacd3f5db40a3c6ddbaa1
Ben Mitchell <bjosephmitchell@gmail.com>
2022-08-13 20:35:04 +0100

    Yo ever heared of nitch?

--- config/zsh/99-motd.zsh
+++ config/zsh/99-motd.zsh
@@ -1,6 +1,7 @@
 motd() {
-    PF_INFO="ascii title os host kernel uptime palette" $(command -v pfetch)
+    #PF_INFO="ascii title os host kernel uptime palette" $(command -v pfetch)
+    nitch
     fortune
 }
 
-[ "$TERM_PROGRAM" != "vscode" ] && motd 
+[ "$TERM_PROGRAM" != "vscode" ] && motd && echo
